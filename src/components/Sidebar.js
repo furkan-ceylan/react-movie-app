ac01420b3ad9d26c5a05c949300f80f84e0259b5
furkan ceylan <ceylan.furkan@outlook.com>
2021-08-26 17:38:54 +0300

    Add Links to routers

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,23 +3,28 @@ import logo from '../logo.png'
 import FavoriteRoundedIcon from '@material-ui/icons/FavoriteRounded'
 import GradeRoundedIcon from '@material-ui/icons/GradeRounded'
 import DateRangeRoundedIcon from '@material-ui/icons/DateRangeRounded'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <img src={logo} className="sidebar-logo" alt="logo" />
       <h3>Discover</h3>
-      <div className="sidebar-item on-route">
-        <FavoriteRoundedIcon />
-        <a href="#">Popular</a>
-      </div>
-      <div className="sidebar-item">
-        <GradeRoundedIcon />
-        <a href="#">Top Rated</a>
-      </div>
+      <Link to="/">
+        <div className="sidebar-item on-route">
+          <FavoriteRoundedIcon />
+          <p>Popular</p>
+        </div>
+      </Link>
+      <Link to="/top-rated">
+        <div className="sidebar-item">
+          <GradeRoundedIcon />
+          <p>Top Rated</p>
+        </div>
+      </Link>
       <div className="sidebar-item">
         <DateRangeRoundedIcon />
-        <a href="#">Upcoming</a>
+        <p>Upcoming</p>
       </div>
     </div>
   )
