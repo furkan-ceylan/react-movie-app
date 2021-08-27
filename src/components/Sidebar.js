84b855ee9345336e9b9351dc325e46a11bea76d5
furkan ceylan <ceylan.furkan@outlook.com>
2021-08-28 01:41:20 +0300

    Check location pathname for class

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,28 +4,50 @@ import FavoriteRoundedIcon from '@material-ui/icons/FavoriteRounded'
 import GradeRoundedIcon from '@material-ui/icons/GradeRounded'
 import DateRangeRoundedIcon from '@material-ui/icons/DateRangeRounded'
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 const Sidebar = () => {
+  const location = useLocation()
+  console.log(location)
+
   return (
     <div className="sidebar">
       <img src={logo} className="sidebar-logo" alt="logo" />
       <h3>Discover</h3>
       <Link to="/">
-        <div className="sidebar-item on-route">
+        <div
+          className={
+            location.pathname === '/' ? 'sidebar-item on-route' : 'sidebar-item'
+          }
+        >
           <FavoriteRoundedIcon />
           <p>Popular</p>
         </div>
       </Link>
       <Link to="/top-rated">
-        <div className="sidebar-item">
+        <div
+          className={
+            location.pathname === '/top-rated'
+              ? 'sidebar-item on-route'
+              : 'sidebar-item'
+          }
+        >
           <GradeRoundedIcon />
           <p>Top Rated</p>
         </div>
       </Link>
-      <div className="sidebar-item">
-        <DateRangeRoundedIcon />
-        <p>Upcoming</p>
-      </div>
+      <Link to="/upcoming">
+        <div
+          className={
+            location.pathname === '/upcoming'
+              ? 'sidebar-item on-route'
+              : 'sidebar-item'
+          }
+        >
+          <DateRangeRoundedIcon />
+          <p>Upcoming</p>
+        </div>
+      </Link>
     </div>
   )
 }
